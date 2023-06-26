e4eb4c09da109cf67a7327aad3f4154a9870e283
ddooo00 <1995thdud@naver.com>
2023-06-26 20:56:14 +0900

    파이어 초기세팅만 됨

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import styled from "styled-components";
 
 import Button from "./ui/Button";
-import { signup } from '../services/authentication'
+import { signup } from "../services/authentication";
 import { authApiAction } from "../redux/slices/apiSlices/authApiSlice/authApiSlice";
 
 const StyledLabel = styled.label``;
@@ -11,45 +11,44 @@ const StyledInput = styled.input``;
 
 const validateInput = (email, pw) => {
   if (!email || !pw) {
-    alert('이메일과 패스워드 모두 입력해 주세요.')
-    return false
+    alert("이메일과 패스워드 모두 입력해 주세요.");
+    return false;
   }
-  return true
-}
+  return true;
+};
 
 const Signup = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const [email, setEmail] = useState('')
-  const [pw, setPw] = useState('')
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
 
   const changeEmailHandler = (e) => {
-    setEmail(e.target.value.trim())
-  }
+    setEmail(e.target.value.trim());
+  };
 
   const changePwHandler = (e) => {
-    setPw(e.target.value.trim())
-  }
+    setPw(e.target.value.trim());
+  };
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    const isValidated = validateInput(email, pw)
-    if (!isValidated) return
+    const isValidated = validateInput(email, pw);
+    if (!isValidated) return;
 
-
-    dispatch(authApiAction.actionRequestedSignup())
-    await signup(email, pw)
-    dispatch(authApiAction.actionSuccessSignup())
-  } 
+    dispatch(authApiAction.actionRequestedSignup());
+    await signup(email, pw);
+    dispatch(authApiAction.actionSuccessSignup());
+  };
 
   return (
     <>
       <form onSubmit={submitHandler}>
-      <StyledLabel htmlFor="email">이메일</StyledLabel>
-      <StyledInput id='email' onChange={changeEmailHandler} value={email} />
-      <StyledLabel htmlFor="pw">비밀번호</StyledLabel>
-      <StyledInput id='pw' onChange={changePwHandler} value={pw} />
-      <Button>제출</Button>
+        <StyledLabel htmlFor="email">이메일</StyledLabel>
+        <StyledInput id="email" onChange={changeEmailHandler} value={email} />
+        <StyledLabel htmlFor="pw">비밀번호</StyledLabel>
+        <StyledInput id="pw" onChange={changePwHandler} value={pw} />
+        <Button>제출</Button>
       </form>
     </>
   );
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,14 +1,16 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyBt6EtUY6FOefd55otOyrnmTlk-huGemcY",
+  authDomain: "login-c9c53.firebaseapp.com",
+  projectId: "login-c9c53",
+  storageBucket: "login-c9c53.appspot.com",
+  messagingSenderId: "921136854294",
+  appId: "1:921136854294:web:c6d145a0790fa8b95e8c37",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
