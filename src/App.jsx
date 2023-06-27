6a237fa619962bb461707efa5d89bfbb3b2c1120
ddooo00 <1995thdud@naver.com>
2023-06-27 12:16:26 +0900

    alert 로그인창만 만들

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
       <h2>music feed</h2>
       <Signup />
+      <Login />
     </>
   );
 }
