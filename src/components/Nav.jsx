6db7f7717c4b3b358dcea9c33cca93ddb63aa7ba
nbcnvc <nbcnvc.dev@gmail.com>
2023-06-27 20:36:03 +0900

    Fix login

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,8 @@
-import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   const isAuthenticated = useSelector((state) => state.authApi.isAuthenticated);
-  const removeSigninLetter = useSelector(
-    (state) => state.authApi.removeSigninLetter
-  );
 
   return (
     <>
@@ -13,14 +10,12 @@ const Nav = () => {
         {isAuthenticated ? (
           <>
             <Link to="auth/profile">profile </Link>
-            <Link to="auth/signout">로그아웃</Link>
+            <Link to="auth/signout">signout</Link>
           </>
         ) : (
           <>
-            <Link to="auth/signup">
-              {removeSigninLetter ? "로그아웃" : "signup"}
-            </Link>
-            {!removeSigninLetter && <Link to="auth/signin">signin</Link>}
+            <Link to="auth/signup">signup</Link>
+            <Link to="auth/signin">signin</Link>
           </>
         )}
       </div>
