d36942e6eddacb9a1e37046cbbb4eb8cd64fb599
ddooo00 <1995thdud@naver.com>
2023-06-27 17:42:46 +0900

    로그인 글씨 사라지게 하기 완료

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,27 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Nav = () => {
-  const isAuthenticated = useSelector(state => state.authApi.isAuthenticated)
+  const isAuthenticated = useSelector((state) => state.authApi.isAuthenticated);
+  const removeSigninLetter = useSelector(
+    (state) => state.authApi.removeSigninLetter
+  );
 
   return (
     <>
       <div>
-        {isAuthenticated ? '로그아웃' : <Link to='auth/signup'>signup </Link>}
-        <Link to='auth/profile'>profile </Link>
-        <Link to='auth/signin'>signin </Link>
+        {isAuthenticated ? (
+          <>
+            <Link to="auth/profile">profile </Link>
+            <Link to="auth/signout">로그아웃</Link>
+          </>
+        ) : (
+          <>
+            <Link to="auth/signup">
+              {removeSigninLetter ? "로그아웃" : "signup"}
+            </Link>
+            {!removeSigninLetter && <Link to="auth/signin">signin</Link>}
+          </>
+        )}
       </div>
     </>
   );
