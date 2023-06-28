52b881c3b4fe6d86cf1e5e169fd848a790b24d93
ddooo00 <1995thdud@naver.com>
2023-06-28 20:08:41 +0900

    사진 프로필 등록까지 완료( 크기는 아직)

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,43 +9,46 @@ import { storage } from "../services/firebase";
 
 const Profile = () => {
   const posts = useSelector((state) => state.postsApi.posts);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [downloadURL, setDownloadURL] = useState("");
 
-  const Profile = () => {
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [downloadURL, setDownloadURL] = useState("");
-
-    const handleFileSelect = (event) => {
-      setSelectedFile(event.target.files[0]);
-    };
-
-    const handleUpload = async () => {
-      const imageRef = ref(storage, `music-web`);
-      await uploadBytes(imageRef, selectedFile);
-
-      const imageURL = await getDownloadURL(imageRef);
-      setDownloadURL(imageURL);
-    };
-
-    useEffect(() => {
-      if (downloadURL !== "") {
-        // 이미지가 업로드된 후에 실행되도록 useEffect를 사용합니다.
-        // downloadURL 값이 변경될 때마다 이미지를 업데이트합니다.
-        const img = new Image();
-        img.src = downloadURL;
-        document.body.appendChild(img);
-        const imageContainer = document.querySelector(".image-container");
-        // 기존의 내용을 지우고 새로운 이미지를 넣습니다.
-        imageContainer.innerHTML = "";
-        imageContainer.appendChild(img);
-      }
-    }, [downloadURL]);
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
   };
+
+  const handleUpload = async () => {
+    const imageRef = ref(storage, `music-web`);
+    await uploadBytes(imageRef, selectedFile);
+
+    const imageURL = await getDownloadURL(imageRef);
+    setDownloadURL(imageURL);
+  };
+
+  const handleEdit = () => {
+    // Define your handleEdit logic here
+  };
+
+  useEffect(() => {
+    if (downloadURL !== "") {
+      // 이미지가 업로드된 후에 실행되도록 useEffect를 사용합니다.
+      // downloadURL 값이 변경될 때마다 이미지를 업데이트합니다.
+      const img = new Image();
+      img.src = downloadURL;
+      document.body.appendChild(img);
+      const imageContainer = document.querySelector(".image-container");
+      // 기존의 내용을 지우고 새로운 이미지를 넣습니다.
+      imageContainer.innerHTML = "";
+      imageContainer.appendChild(img);
+    }
+  }, [downloadURL]);
 
   return (
     <div>
       <UserInfo>
         <h2>로그인 정보</h2>
-        <h3>e-mail : {auth.currentUser.email}</h3>
+        <h3>
+          e-mail : {auth.currentUser ? auth.currentUser.email : "No user"}
+        </h3>
       </UserInfo>
       <h2>내가 작성한 글 보기</h2>
       <Container>
