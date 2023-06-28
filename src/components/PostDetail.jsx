71444f810cd6503868de0e6d2a29469aceb6cedc
ddooo00 <1995thdud@naver.com>
2023-06-28 18:25:00 +0900

    댓글수정 중간저장

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,13 +1,78 @@
-import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const PostDetail = () => {
   const location = useLocation();
   const post = location.state?.post;
+  const navigate = useNavigate();
+  const [editedPost, setEditedPost] = useState(post);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleTitleChange = (event) => {
+    setEditedPost({ ...editedPost, title: event.target.value });
+  };
+
+  const handleArtistChange = (event) => {
+    setEditedPost({ ...editedPost, artist: event.target.value });
+  };
+
+  const handleReviewChange = (event) => {
+    setEditedPost({ ...editedPost, review: event.target.value });
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    setIsEditing(false);
+  };
 
   return (
     <>
-      <h2>{post.title}</h2>
-      <p>{post.review}</p>
+      <h1>오늘의 pick</h1>
+      {isEditing ? (
+        <form>
+          <label>
+            Title:
+            <input
+              type="text"
+              value={editedPost.title}
+              onChange={handleTitleChange}
+            />
+          </label>
+          <br />
+          <label>
+            Artist:
+            <input
+              type="text"
+              value={editedPost.artist}
+              onChange={handleArtistChange}
+            />
+          </label>
+          <br />
+          <label>
+            Review:
+            <textarea
+              value={editedPost.review}
+              onChange={handleReviewChange}
+            ></textarea>
+          </label>
+          <br />
+          <button type="button" onClick={handleSave}>
+            Save
+          </button>
+        </form>
+      ) : (
+        <>
+          <p>Title: {editedPost.title}</p>
+          <p>Artist: {editedPost.artist}</p>
+          <p>Review: {editedPost.review}</p>
+          <button type="button" onClick={handleEdit}>
+            Edit
+          </button>
+        </>
+      )}
     </>
   );
 };
