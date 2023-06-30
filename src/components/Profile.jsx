e81250ecb7148207fc7c92d7d3f4c4b1d0d03747
ddooo00 <1995thdud@naver.com>
2023-06-30 13:44:19 +0900

    프로필 사진 업로드 전 미리보기

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -12,56 +12,62 @@ const Profile = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [downloadURL, setDownloadURL] = useState("");
   const [modalOpen, setModalOpen] = useState(false);
+  const [uploading, setUploading] = useState(false);
 
   // 파일 선택(input type="file") 이벤트가 발생했을 때 호출
   // 선택된 파일을 가져와 setSelectedFile로 UI 업데이트
   const handleFileSelect = (event) => {
-    setSelectedFile(event.target.files[0]);
+    const file = event.target.files[0];
+    setSelectedFile(file);
+
+    // 이미지 미리보기 설정
+    const reader = new FileReader();
+    reader.onload = () => {
+      setDownloadURL(reader.result);
+    };
+    reader.readAsDataURL(file);
   };
 
   // uploadBytes() --> 이미지 업로드
   const handleUpload = async () => {
-    // ref(storage, `본인 폴더명`)
-    const imageRef = ref(storage, `music-web`);
-    await uploadBytes(imageRef, selectedFile);
-    // 이미지 URL 추출
-    const imageURL = await getDownloadURL(imageRef);
-    setDownloadURL(imageURL);
+    try {
+      setUploading(true);
+      const imageRef = ref(storage, `music-web`);
+      await uploadBytes(imageRef, selectedFile);
+      const imageURL = await getDownloadURL(imageRef);
+      setDownloadURL(imageURL);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setUploading(false);
+    }
   };
 
-  // 이미지 화면에 렌더링
   useEffect(() => {
+    // 이미지 미리보기 설정
     if (downloadURL !== "") {
       const imageContainer = document.querySelector(".image-container");
-      const imgBox = document.createElement("div"); // 이미지 박스 요소 생성
-      const img = document.createElement("img"); // 이미지 요소 생성
-
+      const imgBox = document.createElement("div");
+      const img = document.createElement("img");
       img.src = downloadURL;
       imgBox.appendChild(img);
-      imageContainer.innerHTML = ""; // 기존의 내용을 지우고
-
-      // 이미지 박스에 스타일 적용
+      imageContainer.innerHTML = "";
       imgBox.style.maxWidth = "250px";
       imgBox.style.maxHeight = "250px";
       imgBox.style.borderRadius = "50%";
       imgBox.style.margin = "50px 0px 50px 0px";
-      imgBox.style.overflow = "hidden"; // 이미지가 박스를 벗어나지 않도록 설정
-
-      // 이미지에 스타일 적용하여 꽉 차게 설정
+      imgBox.style.overflow = "hidden";
       img.style.width = "100%";
       img.style.height = "100%";
       img.style.objectFit = "cover";
-
       imageContainer.appendChild(imgBox);
     }
   }, [downloadURL]);
 
-  // Edit 버튼 클릭 시, Modal 창 열림
   const handleEdit = () => {
     setModalOpen(true);
   };
 
-  // Modal - Edit 버튼 클릭 시, 이미지 재업로드
   const handleImageEdit = () => {
     if (selectedFile) {
       handleUpload();
@@ -73,9 +79,13 @@ const Profile = () => {
       <div>
         <h1>마이페이지</h1>
         {/* ------ Change Image ------  */}
-        <div className="image-container"></div>
+        <div className="image-container">
+          {selectedFile && <img src={downloadURL} alt="Preview" />}
+        </div>
         <input type="file" onChange={handleFileSelect} />
-        <button onClick={handleUpload}>Upload</button>
+        <button onClick={handleUpload} disabled={uploading}>
+          Upload
+        </button>
         <button onClick={handleEdit}>Edit</button>
 
         {/* ------ Modal ------  */}
@@ -83,7 +93,9 @@ const Profile = () => {
           {modalOpen && (
             <Modal>
               <input type="file" onChange={handleFileSelect} />
-              <button onClick={handleImageEdit}>Image Edit</button>
+              <button onClick={handleImageEdit} disabled={uploading}>
+                Image Edit
+              </button>
               <button onClick={() => setModalOpen(false)}>close</button>
             </Modal>
           )}
@@ -99,25 +111,20 @@ const Profile = () => {
 
       {/* ----- My Post List ------  */}
       <PostContainer>
-        {
-          posts
-            .filter((post) => {
-              const currentUserId = auth.currentUser.uid;
-              return post.userId === currentUserId;
-            })
-            .map((post) => {
-              return (
-                <Card post={post} key={post.id}>
-                  <p>{post.title}</p>
-                  <p>{post.artist}</p>
-                  <p>{post.review}</p>
-                </Card>
-              );
-            })
-          // .map((post) => (
-          //   <Card key={post.id} post={post} />
-          // ))
-        }
+        {posts
+          .filter((post) => {
+            const currentUserId = auth.currentUser.uid;
+            return post.userId === currentUserId;
+          })
+          .map((post) => {
+            return (
+              <Card post={post} key={post.id}>
+                <p>{post.title}</p>
+                <p>{post.artist}</p>
+                <p>{post.review}</p>
+              </Card>
+            );
+          })}
       </PostContainer>
     </Container>
   );
