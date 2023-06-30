24f719bb08e82f2278b0e2e40b075b68cd4dbcb2
nbcnvc <nbcnvc.dev@gmail.com>
2023-06-30 23:03:52 +0900

    Remove duplicated iframe

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -58,24 +58,6 @@ function Posts() {
       ></iframe>
       <NewPost />
       <Container>
-        <iframe
-          style={{
-            // position: "fixed",
-            // zIndex: "-99",
-            width: "100%",
-            height: "650px",
-            // backgroundSize: "cover",
-            marginTop: "0",
-            marginBottom: "auto",
-            opacity: "0.5",
-            pointerEvents: "none",
-            marginBottom: "50px",
-          }}
-          frameborder="0"
-          src="https://www.youtube.com/embed/6ZUIwj3FgUY?mute=1&loop=1&autoplay=1&rel=0&controls=0&showinfo=0"
-          allow="autoplay; encrypted-media"
-          allowfullscreen
-        ></iframe>
         <NewPost />
         <PostContainer>
           {posts.map((post) => (
